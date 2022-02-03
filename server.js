f8468de61276761424772ab564f84b79d3793b6a
NAPS-BIT-Mesra <naps@bitmesra.ac.in>
2022-02-03 11:09:36 +0530

    Delete user endpoint added

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const app = express();
 const mongoose = require("mongoose");
 
 dotenv.config();
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:
+${process.env.MONGODB_PASSWORD}@napsuserdb.n9gvp.mongodb.net/NapsUserDB
+?retryWrites=true&w=majority`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-// mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:
-// ${process.env.MONGODB_PASSWORD}@napsuserdb.n9gvp.mongodb.net/NapsUserDB
-// ?retryWrites=true&w=majority`,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-// mongoose.connection.on("connected",()=>{
-//     console.log("Connected to DB");
-// })
+mongoose.connection.on("connected",()=>{
+    console.log("Connected to DB");
+})
 
 app.listen(3000,()=>{
     console.log("Server listening at port 3000");
