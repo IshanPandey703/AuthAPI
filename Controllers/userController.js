6f2c491d32781d2444df96b01dd8b975825ccd92
NAPS-BIT-Mesra <naps@bitmesra.ac.in>
2022-02-07 19:07:47 +0530

    isActive added and account deactivation feature added, jwt verification also added in some endpoints

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,113 +1,132 @@
-const User = require("../Models/UserModel");
-const { promisify } = require("util");
-const jwt = require("jsonwebtoken");
-const { validationResult } = require("express-validator");
-const bcrypt = require("bcrypt");
+const User = require('../Models/UserModel')
+const { promisify } = require('util')
+const jwt = require('jsonwebtoken')
+const { validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
 
 exports.protect = async (req, res, next) => {
   const user = await User.findOne({ rollNum: req.body.rollNum }).select(
-    "+password"
-  );
+    '+password'
+  )
   if (!user) {
     return res.status(400).json({
-      message: "Either UserName or Password Wrong",
-    });
+      message: 'Either UserName or Password Wrong'
+    })
   }
-  const compare = await user.correctPassword(req.body.password, user.password);
-  if (compare) return next();
+  const compare = await user.correctPassword(req.body.password, user.password)
+  if (compare) return next()
   if (!user || !compare) {
     return res.status(401).json({
-      message: "Either UserName or Password Wrong",
-    });
+      message: 'Either UserName or Password Wrong'
+    })
   }
-};
+}
 
 exports.checkJWT = async (req, res, next) => {
-  let token;
+  let token
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   ) {
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(' ')[1]
   }
   if (req.cookies) {
-    if (req.cookies.jwt) token = req.cookies.jwt;
+    if (req.cookies.jwt) token = req.cookies.jwt
   }
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
 
-  const currentUser = await User.findById(decoded.id);
+  const currentUser = await User.findById(decoded.id)
   if (!currentUser) {
     res.status(401).json({
-      message: "Invalid User",
-    });
+      message: 'Invalid User'
+    })
   }
   if (currentUser.changedPasswordAfter(decoded.iat)) {
     return res.status(400).json({
-      message: "Token Expired, Login again",
-    });
+      message: 'Token Expired, Login again'
+    })
   }
-  req._id = decoded.id;
-  next();
-};
+  req._id = decoded.id
+  next()
+}
 
 exports.UpdateUser = async (req, res, next) => {
-  const { userId, Name, rollNum, profilePhoto } = req.body;
-  let ExistingUser;
+  const { userId, Name, rollNum, profilePhoto } = req.body
+  let ExistingUser
   try {
-    ExistingUser = await User.findOne({ UserId: userId });
+    ExistingUser = await User.findOne({ UserId: userId })
   } catch (err) {
-    next(new Error("User not found!!"));
+    next(new Error('User not found!!'))
   }
-  ExistingUser.name = Name;
-  ExistingUser.rollNum = rollNum;
-  ExistingUser.profilePhoto = profilePhoto;
+  ExistingUser.name = Name
+  ExistingUser.rollNum = rollNum
+  ExistingUser.profilePhoto = profilePhoto
   try {
-    await ExistingUser.save();
+    await ExistingUser.save()
   } catch (err) {
-    next(new Error("User could not be updated!!"));
+    next(new Error('User could not be updated!!'))
   }
-  res.status(200).json({ Message: "User updated successfully", ExistingUser });
-};
+  res.status(200).json({ Message: 'User updated successfully', ExistingUser })
+}
 
 exports.DeleteUser = async (req, res, next) => {
-  const { userId } = req.body;
-  let ExistingUser;
+  const { userId } = req.body
+  let ExistingUser
   try {
-    ExistingUser = await User.findOne({ UserId: userId });
+    ExistingUser = await User.findOne({ UserId: userId })
   } catch (err) {
-    next(new Error("User not found!!"));
+    next(new Error('User not found!!'))
   }
   try {
-    await ExistingUser.remove();
+    await ExistingUser.remove()
   } catch (err) {
-    next(new Error("User deletion failed!!"));
+    next(new Error('User could not be deleted!!'))
   }
-  res.status(200).json({ Message: "User Deleted successfully", ExistingUser });
-};
+  res.status(200).json({ Message: 'User deleted successfully', ExistingUser })
+}
+
+exports.DeactivateUser = async (req, res, next) => {
+  const { userId } = req.body
+  let ExistingUser
+  try {
+    ExistingUser = await User.findOne({ UserId: userId })
+  } catch (err) {
+    next(new Error('User not found!!'))
+  }
+  ExistingUser.isActive = false
+  try {
+    await ExistingUser.save()
+  } catch (err) {
+    next(new Error('User could not be deactivated!!'))
+  }
+  res
+    .status(200)
+    .json({ Message: 'User deactivated successfully', ExistingUser })
+}
 
 exports.updatePassword = async (req, res, next) => {
-  const user = await User.findById(req._id).select("+password");
+  const user = await User.findById(req._id).select('+password')
   if (!req.body.currentPassword)
     return res.status(400).json({
-      status: "fail",
-      message: "Please provide the current password",
-    });
+      status: 'fail',
+      message: 'Please provide the current password'
+    })
   if (!(await user.correctPassword(req.body.currentPassword, user.password)))
     return res.status(400).json({
-      status: "fail",
-      message: "Invalid current Password",
-    });
+      status: 'fail',
+      message: 'Invalid current Password'
+    })
   if (!(await bcrypt.compare(req.body.password, user.password)))
     return res.status(400).json({
-      status: "fail",
-      message: "New password shall not be same as the old one",
-    });
-  user.password = req.body.password;
-  user.cnfrmPassword = req.body.cnfrmPassword;
-  const status = await user.save({ validateBeforeSave: true });
+      status: 'fail',
+      message: 'New password shall not be same as the old one'
+    })
+  user.password = req.body.password
+  user.cnfrmPassword = req.body.cnfrmPassword
+  const status = await user.save({ validateBeforeSave: true })
   res.status(201).json({
-    status: "success",
-    message: "Password Changed",
-  });
-};
+    status: 'success',
+    message: 'Password Changed'
+  })
+}
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,42 +1,65 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("./../Controllers/userController");
-const authController = require("./../Controllers/authController");
-const { check } = require("express-validator");
-router.post("/login", userController.protect, authController.login);
-router.route("/signUp").post(userController.checkJWT, authController.signUp);
-router.post("/login", userController.protect, authController.login);
+const express = require('express')
+const router = express.Router()
+const userController = require('./../Controllers/userController')
+const authController = require('./../Controllers/authController')
+const { check } = require('express-validator')
+router.post('/login', userController.protect, authController.login)
+router.route('/signUp').post(authController.signUp)
 router.patch(
-  "/update",
+  '/update',
+  userController.checkJWT,
   [
-    check("userId").not().isEmpty(),
-    check("Name").not().isEmpty(),
-    check("rollNum").not().isEmpty(),
-    check("profilePhoto").not().isEmpty(),
+    check('userId')
+      .not()
+      .isEmpty(),
+    check('Name')
+      .not()
+      .isEmpty(),
+    check('rollNum')
+      .not()
+      .isEmpty(),
+    check('profilePhoto')
+      .not()
+      .isEmpty()
   ],
   userController.UpdateUser
-);
+)
+router.patch(
+  '/deactivate',
+  userController.checkJWT,
+  [
+    check('userId')
+      .not()
+      .isEmpty()
+  ],
+  userController.DeactivateUser
+)
 router.delete(
-  "/update",
-  [check("userId").not().isEmpty()],
+  '/delete',
+  userController.checkJWT,
+  [
+    check('userId')
+      .not()
+      .isEmpty()
+  ],
   userController.DeleteUser
-);
+)
 router
-  .route("/viewMembers")
-  .get(userController.checkJWT, authController.viewMembers);
+  .route('/viewMembers')
+  .get(userController.checkJWT, authController.viewMembers)
 router.get(
-  "/viewProfile/:userId",
+  '/viewProfile/:userId',
   userController.checkJWT,
   authController.viewProfile
-);
+)
 router.patch(
-  "/updateProfile/:userId",
+  '/updateProfile/:userId',
   userController.checkJWT,
   authController.updateProfile
-);
+)
 router.patch(
-  "/updatePassword",
+  '/updatePassword',
   userController.checkJWT,
   userController.updatePassword
-);
-module.exports = router;
+)
+module.exports = router
